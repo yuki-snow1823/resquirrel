78b87ec5fe9646ff0c82f5244de52f6611799fa4
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-04-27 23:55:11 +0900

    fix path

--- lib/resquirrel.rb
+++ lib/resquirrel.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "resquirrel/version"
-require_relative "client/open_ai"
+require_relative "client/openai"
 require_relative "client/notion"
 require "json"
 require "logger"
