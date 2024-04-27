39cfbe1df8a9de973825d658909d7da93c2205a5
Yuki Horikoshi <59280290+yuki-snow1823@users.noreply.github.com>
2024-04-27 15:38:41 +0900

    Revert "Tested notion prop URL, prompt once in Japanese"

--- lib/resquirrel/main.rb
+++ lib/resquirrel/main.rb
@@ -21,9 +21,6 @@ response = openai_client.summary_pr(title, body)
 summary = response["choices"].first["message"]["content"]
 url = pr_data["url"]
 
-p url
-p "---test----"
-
 logger.info "Updating Notion database..."
 
 notion_client = NotionClient.new(ENV["NOTION_API_KEY"], ENV["NOTION_DATABASE_ID"])
