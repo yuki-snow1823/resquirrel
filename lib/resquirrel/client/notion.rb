c6061fc4b716669164cf862fce1386574b1a74e9
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-04-27 13:40:18 +0900

    Test to see if data can be sent to notion

---
+++ lib/resquirrel/client/notion.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+require 'json'
+
+class NotionClient
+  def initialize(api_key, database_id)
+    @api_key = api_key
+    @database_id = database_id
+  end
+
+  def update_database(summary)
+    uri = URI("https://api.notion.com/v1/pages")
+    request = Net::HTTP::Post.new(uri)
+    request["Authorization"] = "Bearer #{@api_key}"
+    request.content_type = 'application/json'
+    request.body = {
+      parent: { type: "database_id", database_id: @database_id },
+      properties: {
+        title: { title: [{ text: { content: summary } }] }
+      }
+    }.to_json
+
+    Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+  end
+end
