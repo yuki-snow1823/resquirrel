763b83b2fc918b62195928d6a6ac7e41ea61a066
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-04-27 13:44:23 +0900

    event data can now be retrieved by testing

--- lib/resquirrel/main.rb
+++ lib/resquirrel/main.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 require 'json'
-require_relative 'client/notion'
+require_relative 'lib/resquirrel/client/notion'
 
 event_data = JSON.parse(File.read(ENV["GITHUB_EVENT_PATH"]))
 
 p event_data
 
-NotionClient.new(ENV["NOTION_API_KEY"], ENV["NOTION_DATABASE_ID"]).update_database(event_data["pull_request"]["title"])
+NotionClient.new(ENV["NOTION_API_KEY"], ENV["NOTION_DATABASE_ID"]).update_database(event_data["commits"].map { |commit| commit["message"] })
